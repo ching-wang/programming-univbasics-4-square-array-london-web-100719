5fbfacb7456addeb4cfb7582259354f8cc12ebbd
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 19:03:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,12 +7,3 @@ def square_array(num)
    counter += 1
  end
 end 
-
-
-def square_array(numbers)
-  count = 0
-  while count < numbers.length do
-    numbers.index ** 2 
-  end
-  square_array(numbers)
-end 
