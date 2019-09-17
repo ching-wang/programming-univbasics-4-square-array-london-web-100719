3f23a9e4b34c3ae75d176bdbcd76c3062ecdbc47
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 19:40:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,21 +1,9 @@
-def square_array(array)
+def square_array(numbers)
   new_array = []
   counter = 0 
- while array < array.length 
-   if array.index ** 2  === counter
-   counter.push(new_array)
+ while numbers < numbers.length()
+  new_array.push(numbers[counter] ** numbers[counter])
    counter += 1
-   return new_array
    end 
- end
-end
-
-def square_array(numbers)
-    new_array = []
-    index = 0
-    while index < numbers.length()
-        new_array.push(numbers[index] ** numbers[index])
-        index += 1
-    end
-    return new_array
+  return new_array
 end
