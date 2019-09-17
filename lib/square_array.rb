fab8b2aba7bb936fa66a076606c1510722ddd345
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 19:17:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,9 +8,3 @@ def square_array(num)
    return new_array
   end
  end
- 
- 
-
-Write a statement that squares the array element whose index is the same as your counter, and pushes that result into the array you created in step 1.
-Increment your counter by 1 (you forgot to do that, so you'll be getting an endless loop since count will always equal zero).
-Return the array you created in step 1.
