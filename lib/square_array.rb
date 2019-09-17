a64faa6190d6a4d323815d9d98dd71f934a8b9df
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 19:17:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,15 +2,15 @@ def square_array(num)
   new_array = []
   counter = 0 
  while num < num.length 
-   if num.index ** 2 === counter
+   if square_array([num]) === counter
    counter.push(new_array)
    counter += 1
+   return new_array
   end
  end
  
  
- 
-Set up your while loop (as you have it, except you don't need the do at the end).
+
 Write a statement that squares the array element whose index is the same as your counter, and pushes that result into the array you created in step 1.
 Increment your counter by 1 (you forgot to do that, so you'll be getting an endless loop since count will always equal zero).
 Return the array you created in step 1.
