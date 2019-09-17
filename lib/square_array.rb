1cdb2ef28d882da27a93b16cabb799a36dcbe306
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 19:38:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,3 +9,13 @@ def square_array(array)
    end 
  end
 end
+
+def square_array(numbers)
+    new_array = []
+    index = 0
+    while index < numbers.length()
+        new_array.push(numbers[index] ** numbers[index])
+        index += 1
+    end
+    return new_array
+end
